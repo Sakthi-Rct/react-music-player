5a46bd5e17d61797379650277429a511b7e5f44d
sakthivel-happyfox <sakthivel@happyfox.com>
2021-05-22 12:52:44 +0530

    New songs updated

--- src/data.js
+++ src/data.js
@@ -61,6 +61,36 @@ function chillHop() {
       id: uuidv4(),
       active: false,
     },
+    {
+      name: "Pandian",
+      cover:
+      "https://a10.gaanacdn.com/gn_img/albums/Oxd3xzPbgV/xd3xw8p9Kg/size_xxl.webp",
+      artist: "Ilayaraja",
+      audio: "https://dslink.xyz/Masstamilan.In/Old/Pandiyan-Masstamilan.In/Anbe-Neeyene-MassTamilan.com.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Panakkaran",
+      cover:
+      "https://i2.cinestaan.com/image-bank/1500-1500/80001-81000/80806.jpg",
+      artist: "Ilayaraja",
+      audio: "https://masstamilandownload.com/tamil/1990/Panakkaran-128kbps/Ding-Dang-Dang-%5BMasstamilan.in%5D.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Mappillai",
+      cover:
+      "https://jiocinemaweb.cdn.jio.com/jioimages.cdn.jio.com/content/entry/dynamiccontent/thumbs/512/512/0/67/66/1e5a5e70673411e99e973b54fdd75759_1557476010272_p_medium.jpg",
+      artist: "Ilayaraja",
+      audio: "https://dslink.xyz/Masstamilan.In/Old/Mappillai-Masstamilan.In/Maanin-Iru-Kangal-MassTamilan.io.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
     //ADD MORE HERE
   ];
 }
